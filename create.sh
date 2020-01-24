d0f54c52834c18bdfb863112b76660a8a78b9452
Emmanuel Nyachoke <achachiez@gmail.com>
2020-01-24 08:46:41 +0300

    Increased readiness probe time to 100 seconds

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
 #Docs command don't excute
 echo "Docs command don't excute"
 exit 0
-argocd app create argocd-test --upsert --repo https://github.com/livinggoods/lg-cd-configs.git --path jsonnet-cht-core-platform --dest-server https://kubernetes.default.svc --dest-namespace medic  --jsonnet-tla-str 'name=${ARGOCD_APP_NAME}' --jsonnet-tla-str 'couchDbServer=${ARGOCD_APP_NAME}-couchdb.${ARGOCD_APP_NAMESPACE}.svc.cluster.local' --jsonnet-tla-str 'chtCoreServer=${ARGOCD_APP_NAME}-chtcore.${ARGOCD_APP_NAMESPACE}.svc.cluster.local'
+argocd app create argocd-test2 --upsert --repo https://github.com/livinggoods/lg-cd-configs.git --path jsonnet-cht-core-platform --dest-server https://kubernetes.default.svc --dest-namespace medic  --jsonnet-tla-str 'name=${ARGOCD_APP_NAME}' --jsonnet-tla-str 'couchDbServer=${ARGOCD_APP_NAME}-couchdb.${ARGOCD_APP_NAMESPACE}.svc.cluster.local' --jsonnet-tla-str 'chtCoreServer=${ARGOCD_APP_NAME}-chtcore.${ARGOCD_APP_NAMESPACE}.svc.cluster.local'
